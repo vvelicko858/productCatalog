9291d7193fe48d961df417c34dca631aa8e47146
Ivan Vialichka <vvelicko858@gmail.com>
2025-08-16 11:51:38 +0300

    init project + architecture

---
+++ src/app/features/categories/pages/categories-list/categories-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-list',
+  imports: [],
+  templateUrl: './categories-list.component.html',
+  styleUrl: './categories-list.component.scss'
+})
+export class CategoriesListComponent {
+
+}
---
+++ src/app/shared/components/form-field/form-field.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  imports: [],
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.scss'
+})
+export class FormFieldComponent {
+
+}
